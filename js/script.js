27b1dbd5ad8c9412cb857ce388c406f63cb8343a
PlayzaCat <playza@mail.ru>
2022-12-19 19:44:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+console.log('Привет!!!');
+new Swiper('.wrapper');
+
+
+
+
+
+
+  var swiper2 = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+   
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+
+  });
+
+
+// 
+
+// Readmore block
+
+
+
+let content = document.querySelector(".logo-block")
+let show = document.querySelector('.readmore__open');
+let hide = document.querySelector('.readmore__close');
+
+hide.addEventListener("click", () => {
+   content.style.height = '180px';
+})
+
+show.addEventListener("click", () => {
+   content.style.height = 'auto';
+})
+
+
+
+const menu = document.querySelector('.readmore');
+const open = document.querySelector('.readmore__open');
+const close = document.querySelector('.readmore__close');
+
+close.addEventListener('click', () => {
+    menu.classList.remove('readmore__open');
+  });
+
+
+open.addEventListener('click', () => {
+  menu.classList.add('readmore__open');
+});
+
+
+
+
+
